73c7abd2cbb1ee1925e3c23a3697540742839231
Henrique Costa <henrique.costa@kbase.com.br>
2024-04-09 21:58:23 -0300

    metodos rest

---
+++ src/test/java/com/henrique/avengers/AvengersApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.henrique.avengers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvengersApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
